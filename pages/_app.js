c9e7be4a6eb64715fdf94883f3a6e2e75a62b5fc
Agmar Putra <agmarputra@gmail.com>
2021-12-30 14:48:15 +0700

    Using display name

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,24 @@ import '../styles/globals.css'
 import { createContext } from 'react'
 
 export const MyContext = createContext()
+export const OtherContext = createContext()
+
+MyContext.displayName = 'MyDisplayName'
+OtherContext.displayName = 'OtherDisplayName'
+
+console.log(MyContext)
+console.log(OtherContext)
+
+/*
+Context object accepts a displayName string property. 
+React DevTools uses this string to determine what to display for the context.
+
+For example, the following component will appear as MyDisplayName in the DevTools:
+
+<MyContext.Provider> // "MyDisplayName.Provider" in DevTools
+<MyContext.Consumer> // "MyDisplayName.Consumer" in DevTools
+
+*/
 
 function MyApp({ Component, pageProps }) {
   return (
