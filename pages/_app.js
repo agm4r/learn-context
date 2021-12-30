72f79a826f39152dcbf67f3c98d4647199964be8
Agmar Putra <agmarputra@gmail.com>
2021-12-30 11:33:04 +0700

    add Commentar

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,13 @@ import '../styles/globals.css'
 import { createContext } from 'react'
 
 const myContext = createContext()
+// Above is example how to using create Context
+/*
+  When React renders a component that subscribes to this Context object 
+  it will read the current context value from the closest 
+  matching "Provider" above it in the tree.
+*/
+// You can Also set the default value like this " const myContext = createContext(defaultValue) "
 
 function MyApp({ Component, pageProps }) {
   return (
