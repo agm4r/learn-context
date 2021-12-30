9937c33675fd10ff4a3f85ecfcead8ca35e59ef1
Agmar Putra <agmarputra@gmail.com>
2021-12-30 11:26:42 +0700

    add CreateContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,12 @@
-import ThemeProvider from '../src/ThemeContext'
 import '../styles/globals.css'
+import { createContext } from 'react'
 
+const myContext = createContext()
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Component {...pageProps} />
   )
 }
 
 export default MyApp
-
-
-//Uncomment this to see Context in Class Component
-
-// export const ThemeContext = createContext()
-
-// function MyApp({ Component, pageProps }) {
-//   const [darkTheme, setDarkTheme] = useState(true)
-  
-//   return (
-//     <ThemeContext.Provider value={darkTheme}>
-//       <button onClick={() => setDarkTheme(!darkTheme)}>Click</button>
-//       <Component {...pageProps} />
-//     </ThemeContext.Provider>
-//   )
-// }
-
-// export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,3 @@
-import { useTheme, useThemeUpdate } from "../src/ThemeContext"
-
 export default function Home() {
-  const darkTheme = useTheme()
-  const toggleTheme = useThemeUpdate()
-
-  const styles = {
-    display: 'grid',
-    placeItems: 'center',
-    minHeight: '100vh',
-    background: darkTheme ? '#002' : '#fff',
-    color: darkTheme ? '#0fc' : '#002',
-    transition: '.9s',
-    cursor: 'pointer'
-  }
-
-  return (
-    <div onClick={toggleTheme} style={styles}>
-      <h1>Learn Context</h1>
-    </div>
-  )
+  return <h1>Create Context Example</h1>;
 }
