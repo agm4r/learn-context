869b9f2c24dfc9c8f0a968d5937f83cf904a0d52
Agmar Putra <agmarputra@gmail.com>
2021-12-30 14:32:48 +0700

    using Context.Consumer

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,13 @@
 import '../styles/globals.css'
 import { createContext } from 'react'
 
-export const myContext = createContext()
-/*
-  Every Context object comes with a Provider React component 
-  that allows consuming components to subscribe to context changes.
-*/
+export const MyContext = createContext()
 
 function MyApp({ Component, pageProps }) {
   return (
-    <myContext.Provider value={{ name: 'Agmar' }}>
-      {
-        /*
-          The Provider component 
-          accepts a value prop to be passed to consuming 
-          components that are descendants of this Provider
-
-          One Provider can be connected to many consumers. 
-          Providers can be nested to override values deeper within the tree.
-
-          NOTE: To see how to use the current value, 
-                you can move to branch feature/consumer 
-                then to page/index.js file. 
-        */
-      }
+    <MyContext.Provider value={{ name: 'Agmar' }}>
       <Component {...pageProps} />
-    </myContext.Provider>
+    </MyContext.Provider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import { MyContext } from "./_app"
+
 export default function Home() {
-  return <h1>Create Context Example</h1>;
+  return (
+    <MyContext.Consumer>
+      {
+        value => <div><h1>{ JSON.stringify(value) }</h1></div>
+      }
+    </MyContext.Consumer>
+  );
 }
+
+/*
+  A React component that subscribes to context changes. 
+  Using this component lets you subscribe to a context within a function component.
+
+  Requires a function as a child. The function receives 
+  the current context value and returns a React node. 
+  The value argument passed to the function will be equal to the value prop 
+  of the closest Provider for this context above in the tree. If there 
+  is no Provider for this context above, the value argument will be equal 
+  to the defaultValue that was passed to createContext().
+*/
