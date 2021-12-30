883fd67156f07076d41a4ac8f3b0837feba6a961
Agmar Putra <agmarputra@gmail.com>
2021-12-30 13:31:32 +0700

    Using Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import '../styles/globals.css'
 import { createContext } from 'react'
 
-const myContext = createContext()
+export const myContext = createContext()
+/*
+  Every Context object comes with a Provider React component 
+  that allows consuming components to subscribe to context changes.
+*/
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <myContext.Provider value={{ name: 'Agmar' }}>
+      {
+        /*
+          The Provider component 
+          accepts a value prop to be passed to consuming 
+          components that are descendants of this Provider
+
+          One Provider can be connected to many consumers. 
+          Providers can be nested to override values deeper within the tree.
+
+          NOTE: To see how to use the current value, 
+                you can move to branch feature/consumer 
+                then to page/index.js file. 
+        */
+      }
+      <Component {...pageProps} />
+    </myContext.Provider>
   )
 }
 
